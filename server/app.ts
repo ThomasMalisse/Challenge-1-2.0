c823086a840c643670a40df6699aa16df29f2be0
ThomasMalisse <thomas.malisse@student.arteveldehs.be>
2026-03-07 13:08:54 +0100

    2.0 V1

---
+++ server/app.ts
@@ -0,0 +1,22 @@
+import express, { Application, Request, Response } from "express";
+import path from "path";
+import routes from "./routes";
+
+const app: Application = express();
+const PORT: number = parseInt(<string>process.env.PORT, 10) || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static(path.join(__dirname, "/public")));
+app.use("/", routes);
+
+app.get("/", (req: Request, res: Response): void => {
+  res.sendFile(path.join(__dirname, "views", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server draait op http://localhost:${PORT}`);
+});
---
+++ server/routes.ts
@@ -0,0 +1,51 @@
+import express, { Request, Response } from "express";
+import path from "path";
+import news from "./data/news.json";
+
+const router = express.Router();
+
+// index
+router.get("/", (req: Request, res: Response) => {
+  res.render("index", { news });
+});
+
+// detail
+router.get("/news/:slug", (req: Request, res: Response) => {
+  const slug = req.params.slug;
+
+  const article = news.find((n) => n.slug === slug);
+
+  if (!article) {
+    return res.status(404).render("404");
+  }
+
+  res.render("details", { article });
+});
+
+// add pagina
+router.get("/add", (req: Request, res: Response) => {
+  res.render("add");
+});
+
+// POST artikel toevoegen
+router.post("/add", (req: Request, res: Response) => {
+  const { title, slug, content } = req.body;
+
+  const newArticle = {
+    title,
+    slug,
+    content,
+    date: new Date().toISOString().split("T")[0],
+  };
+
+  news.push(newArticle);
+
+  res.redirect("/");
+});
+
+// 404
+router.use((req: Request, res: Response) => {
+  res.status(404).render("404");
+});
+
+export default router;
